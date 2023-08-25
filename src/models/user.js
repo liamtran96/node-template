286d608c11a83191a0923da46f0e4ac6967f70bb
LiamTrn <liamtrn@gmail.com>
2023-08-25 16:15:57 +0700

    ci: update packages

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
